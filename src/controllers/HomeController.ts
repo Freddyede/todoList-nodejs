d8d82ec4f4ac98dd42dc65aeeae2789a64203198
Freddyede <patouillardfranck3@gmail.com>
2024-01-08 18:37:35 +0100

    home/index home.index

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,6 +2,4 @@ import { User } from "../entity/User";
 import { getRelationsFinder } from "../utils";
 import { Config } from "../config";
 
-export const index = async (_req: any, res: any): Promise<void> => {
-    res.render('home/index', {titre: "Dashboard", users: await getRelationsFinder(User, Config.entities.user.relations), years: new Date().getFullYear().toString()});
-}
+export const index = async (_req: any, res: any): Promise<void> => res.render('home.index', {titre: "Dashboard", users: await getRelationsFinder(User, Config.entities.user.relations), years: new Date().getFullYear().toString()});
