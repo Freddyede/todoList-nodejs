3c0bcf5bd981a280c2b926719ba483372b4d2503
Freddyede <patouillardfranck3@gmail.com>
2024-01-08 15:08:15 +0100

    Updated views and added public directories

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,7 +1,7 @@
 import { User } from "../entity/User";
-import { Tasks } from "../entity/Tasks";
 import { getRelationsFinder } from "../utils";
+import { Config } from "../config";
 
-export const index = async (_req: any, res: any) => {
-    res.render('home/index', {titre: "Dashboard", users: await getRelationsFinder(User)});
+export const index = async (_req: any, res: any): Promise<void> => {
+    res.render('home/index', {titre: "Dashboard", users: await getRelationsFinder(User, Config.entities.user.relations), years: new Date().getFullYear().toString()});
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,9 @@
 import { AppDataSource } from "../data-source";
 import  { EntityTarget } from "typeorm";
-import { Config } from "../config";
 import { app } from "../constants";
 import HomeRoads from "../routes/HomeRoads";
 
-export const getRelationsFinder = async (entityName: EntityTarget<any>): Promise<any> => await AppDataSource.getRepository(entityName).find({relations: { ...Config.entities.user.relations }});
+export const getRelationsFinder = async (entityName: EntityTarget<any>, relations: any): Promise<any> => await AppDataSource.getRepository(entityName).find({relations: { ...relations }});
 
 /**
  * Runner app.use
